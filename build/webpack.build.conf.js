23160a34833514ff2a5807303289747dfcab55a3
Daria Kozhemiakina <ihavebadfantasy@protonmail.com>
2019-12-30 13:38:19 +0200

    created production folder and config

--- build/webpack.build.conf.js
+++ build/webpack.build.conf.js
@@ -1,3 +1,5 @@
+/*
+// to build a demo in docs folder
 const merge = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.conf')
 
@@ -10,3 +12,19 @@ const buildWebpackConfig = merge(baseWebpackConfig, {
 module.exports = new Promise((resolve, reject) => {
   resolve(buildWebpackConfig)
 })
+*/
+
+// to build plugin prod folder
+const merge = require('webpack-merge')
+const prodWebpackConfig = require('./webpack.prod.conf')
+
+const buildWebpackConfig = merge(prodWebpackConfig, {
+  // BUILD config
+  mode: 'production',
+  plugins: []
+})
+
+module.exports = new Promise((resolve, reject) => {
+  resolve(buildWebpackConfig)
+})
+
---
+++ build/webpack.prod.conf.js
@@ -0,0 +1,96 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const PATHS = {
+  src: path.join(__dirname, '../src'),
+  dist: path.join(__dirname, '../prod'),
+  assets: './'
+}
+
+module.exports = {
+  externals: {
+    paths: PATHS
+  },
+  entry: {
+    rotateDevice: PATHS.src,
+  },
+  output: {
+    filename: `${PATHS.assets}js/[name].js`,
+    path: PATHS.dist,
+    publicPath: './'
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  module: {
+    rules: [{
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]'
+        }
+      }, {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          }, {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              config: {
+                path: `${PATHS.src}/js/postcss.config.js`
+              }
+            }
+          }, {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }, {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          }, {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              config: {
+                path: `${PATHS.src}/js/postcss.config.js`
+              }
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: `${PATHS.assets}css/[name].css`,
+    }),
+  ],
+}
